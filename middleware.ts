5e7f6672a39ae21f44d0dd9024aebf21c87b2840
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2025-12-04 00:24:01 +0300

    feat (set up next-intl environments)

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import createMiddleware from "next-intl/middleware";
+import { routing } from "@/i18n/routing";
+
+const intlMiddleware = createMiddleware(routing);
+
+export default auth((req) => {
+  return intlMiddleware(req);
+});
+
+export const config = {
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
